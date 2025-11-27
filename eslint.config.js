59e3bd658af66f0408b67e0dce64faabeaa7d19a
Martial Hilarion Avadra <martialhilarionavadra@gmail.com>
2025-11-27 09:19:47 +0100

    update build command 4

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,5 @@
-// eslint.config.js
-import next from 'eslint-config-next';
-
 export default [
-  ...next,
   {
-    ignores: ['lib/generated/**'], // 👈 Ignorer les fichiers générés ici
+    ignores: ["**/*"],
   },
 ];
